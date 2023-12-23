6c6411ff897e18c7f29dc55e3934f21712a0f52c
Julia Popova <yuliarandomchenko@gmail.com>
2023-12-23 19:31:16 +0300

    Init project

---
+++ DrawingApp/DrawingApp/DrawingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingAppApp.swift
+//  DrawingApp
+//
+//  Created by Julia Popova on 23.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
